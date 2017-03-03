137f93771a9d77be5670f5b963e2a66146a6d17d
Arthur Scharf <arthur.scharf@gmail.com>
2017-03-03 19:01:23 +0100

    added appendix. changed name of matlab file

--- code/proc_water.m
+++ code/proc_water.m
@@ -1,5 +1,7 @@
 clear all; close all; clc;
 
+% this file runs a kNN algorithm using pre-set classes (manually done)
+
 srcfiles = dir('imagedata/alldata/*cropped_refd.png');
 
 R = zeros(1,length(srcfiles),'uint32');
@@ -28,43 +30,45 @@ for i = 1 : length(srcfiles)
         142 162 101 3;
         125 158 93 3;
         125 150 98 3;] ;
-    
-    
+
+
     NbData = size(DataBase, 1) ;
-    
+
     Pixels = DataBase(:,1:3) ;
-    
+
     Classes = DataBase(:,4) ;
-    
+
     MaskClasses = zeros(U,V) ;
-    
+
     for u = 1 : U
         for v = 1 : V
-            
+
             r = I(u,v,1) ;
             g = I(u,v,2) ;
             b = I(u,v,3) ;
-            
+
             TabRGB = repmat([r g b],NbData,1) ;
             D2 = (TabRGB - Pixels).^2 ;
+            % minimise distance
             [valmin,posmin] = min( sum(D2') ) ;
             MaskClasses(u,v) = Classes(posmin) ;
-            
-            
+
+
         end
     end
+    % plot the water classe, for this report the others aren't used.
     show = (MaskClasses==1);
     bordermask = bwmorph(rgb2gray(I),'erode',1);
     show = show .* bordermask;
     show(:,:,2) = show;
     show(:,:,3) = show(:,:,1);
     waterMasked = show.*I;
-    figure, 
+    figure,
     subplot(1,2,1),imshow(X)
     subplot(1,2,2),imshow(show.*I)
     [pathstr,name,ext] = fileparts(filename);
     imwrite(show.*I,strcat('imagedata/analysis/water',name,'_w',ext));
-    
+
     % now reshape and count pixels
     [r col] = size(waterMasked);
     BW_resh =reshape(waterMasked,1,r*col);
@@ -80,3 +84,5 @@ for i = 1 : length(srcfiles)
     % append it to a matrix
     R(i) = cnt;
 end
+
+plot(R)
